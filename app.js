e71d2bfe24f80f32a724c48b9375dddef9788477
Isaac Wasserman <isaacrwasserman@gmaill.com>
2019-02-17 02:56:25 -0500

    new challenge

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/.well-known/acme-challenge/jRnmVHVRi-c-iIVePL3z5WD-_bkUZrlH9MXYqZANxaM', challengeRouter);
+app.use('/.well-known/acme-challenge/gu9Ch8wyVjqXNCSqHGX14qCuThxmFPqXX2gQrdvGS58', challengeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/SSLChallenge.js
+++ routes/SSLChallenge.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.send('jRnmVHVRi-c-iIVePL3z5WD-_bkUZrlH9MXYqZANxaM.Gwqggj3v6yHZ9_vjYqU0baqCctreLRNDo5uL8f-ZLV0');
+  res.send('gu9Ch8wyVjqXNCSqHGX14qCuThxmFPqXX2gQrdvGS58.Gwqggj3v6yHZ9_vjYqU0baqCctreLRNDo5uL8f-ZLV0');
 
 });
 
